d56f3b85d61a1dd28c530d29255425dc12abf28a
Salome Treichler <salome.treichler@noseryoung.ch>
2020-04-09 14:16:32 +0200

    lambda basics with functional Interface

--- extended/src/main/java/ch/course223/extended/ExtendedApplication.java
+++ extended/src/main/java/ch/course223/extended/ExtendedApplication.java
@@ -2,9 +2,8 @@ package ch.course223.extended;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class ExtendedApplication {
 
 	public static void main(String[] args) {
---
+++ extended/src/main/java/ch/course223/extended/MathematicalOperation.java
@@ -0,0 +1,8 @@
+package ch.course223.extended;
+
+import java.util.List;
+
+@FunctionalInterface
+public interface MathematicalOperation {
+    int executeNumericalOperation (int x, int y);
+}
